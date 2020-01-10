adea70e41371702f227b93f9b913e2c55cdc36a3
shuaiwuLi <shuaiwu123@foxmail.com>
2020-01-11 01:11:06 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,17 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
   chainWebpack: config => {
     config.resolve.alias
     // 配置快捷路径，styles为路径名字，resolve是原路径地址
       .set('@styles', resolve('src/assets/styles'))
     // 组件调用快捷路径
       .set('@common', resolve('src/components/common'))
+    config.devServer
+      // ... your configs
+      .set('disableHostCheck', true) // 加上这段
   }
 }
